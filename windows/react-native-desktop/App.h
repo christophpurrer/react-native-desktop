a6314a9b988ae3e04e77442199891d457624a04d
Christoph Purrer <christophpurrer@gmail.com>
2019-12-20 10:46:18 -0800

    Add react-native-windows

---
+++ windows/react-native-desktop/App.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "App.xaml.g.h"
+
+
+
+namespace winrt::rnw::implementation
+{
+    struct App : AppT<App>
+    {
+        App() noexcept;
+    };
+} // namespace winrt::rnw::implementation
+
+
